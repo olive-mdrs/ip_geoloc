d6cf9bcfc688a635be2d0f2bda471df2ee5f7342
olive-mdrs <olivemedeiros68@gmail.com>
2026-06-01 21:19:59 -0300

    adicionando arquivos iniciais da UI, utilizando a biblioteca textual

---
+++ ui/interface.py
@@ -0,0 +1,10 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer, Input, Button, RichLog
+from textual.containers import Horizontal, Vertical
+from textual import work
+
+from core.network import buscar_dados_ip
+
+class GeolocalizadorApp(App):
+    TODO
+
